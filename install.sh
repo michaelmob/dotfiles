d80ded918a9ea7aa465d80452ac7d46f4991a522
thetarkus <thetarkus@users.noreply.github.com>
2018-04-27 18:55:38 -0400

    Remove unused configs Update neovim configs

--- install.sh
+++ install.sh
@@ -2,31 +2,28 @@
 DIR=$(readlink -f "${0%/*}/")
 
 l () {
-	# Test target exists
-	if [ -e "$2" ]; then
-		if [ -L "$2" ]; then
-			# Link already exists
-			echo "$2 already linked"
-			return 0
-		else
-			# Exists, but not link? Prompt deletion
-			echo -e "\n\e[7m!!! $2 already exists !!!\e[0m"
-			rm -rI "$2" && [ -e "$2" ] && return 1
-		fi
-	fi
+    # Test target exists
+    if [ -e "$2" ]; then
+        if [ -L "$2" ]; then
+            # Link already exists
+            echo "$2 already linked"
+            return 0
+        else
+            # Exists, but not link? Prompt deletion
+            echo -e "\n\e[7m!!! $2 already exists !!!\e[0m"
+            rm -rI "$2" && [ -e "$2" ] && return 1
+        fi
+    fi
 
-	# Create symbolic link
-	ln -s "$1" "$2" && echo "Linked $1 -> $2"
+    # Create symbolic link
+    ln -s "$1" "$2" && echo "Linked $1 -> $2"
 }
 
 
 # Delete dead symlinks
 # find -L $DIR -maxdepth 3 -type l -delete
 
-l $DIR/bashrc			$HOME/.bashrc
-l $DIR/nvim			$HOME/.config/nvim
-l $DIR/vscode			$HOME/.config/Code\ -\ OSS
-l $DIR/sublime-text-3		$HOME/.config/sublime-text-3
-l $DIR/ideavimrc		$HOME/.ideavimrc
-l $DIR/osbuddy 			$HOME/.osbuddy
-l $DIR/pypirc 			$HOME/.pypirc
+l $DIR/bashrc       $HOME/.bashrc
+l $DIR/nvim         $HOME/.config/nvim
+l $DIR/ideavimrc    $HOME/.ideavimrc
+l $DIR/pypirc       $HOME/.pypirc
